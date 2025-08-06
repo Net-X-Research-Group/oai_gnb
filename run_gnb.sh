4df42796b3444c89167169d9ea705370d77cac8f
Robert Hayek <hayekr2@gmail.com>
2025-08-06 11:11:55 -0500

    Add note to script

--- run_gnb.sh
+++ run_gnb.sh
@@ -1,1 +1,3 @@
+# This only runs the source version of oai-gnb, not a docker entry point
+
 sudo ../openairinterface5g/cmake_targets/ran_build/build/nr-softmodem -O gnb.sa.band78.fr1.106PRB.usrpx310_nu.conf --usrp-tx-thread-config 1 -E --continuous-tx 
